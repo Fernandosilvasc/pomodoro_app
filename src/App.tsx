ba582ac9e3cffdec7a57441e37de22ad88ca4286
Fernandosilvasc <fernando_silvasc@hotmail.com>
2023-04-07 09:40:32 -0700

    feat: Creates button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components";
-// import { Button } from "./components/Button";
+import { Button } from "./components/Button";
 
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -7,12 +7,11 @@ import { defaultTheme } from "./styles/themes/default";
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Test</h1>
-      {/* <Button variant='primary' />
+      <Button variant='primary' />
       <Button variant='secondary' />
       <Button variant='success' />
       <Button variant='danger' />
-      <Button /> */}
+      <Button />
 
       <GlobalStyle />
     </ThemeProvider>
