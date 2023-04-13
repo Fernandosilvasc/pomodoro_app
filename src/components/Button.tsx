1c7020e4d40d6fd6859cd52d82e72c1a783a1b08
Fernandosilvasc <fernando_silvasc@hotmail.com>
2023-04-12 21:22:26 -0700

    refactor: Remove unused components.

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, type ButtonVariant } from './Button.styles';
-
-interface ButtonProps {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = 'primary' }: ButtonProps): JSX.Element {
-  return <ButtonContainer variant={variant}>Send</ButtonContainer>;
-}
